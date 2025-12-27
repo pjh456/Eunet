a8fcfbf1b7f6e7a62d39b0b33f3833e30d866aa5
爱特小登队 <t202510423998135@eduxiji.net>
2025-12-28 07:54:44 +0800

    feat: 添加 Scenario 基类封装

---
+++ include/eunet/core/scenario.hpp
@@ -0,0 +1,20 @@
+#ifndef INCLUDE_EUNET_CORE_SCENARIO
+#define INCLUDE_EUNET_CORE_SCENARIO
+
+#include "eunet/util/result.hpp"
+#include "eunet/core/orchestrator.hpp"
+
+namespace core::scenario
+{
+    class Scenario
+    {
+    public:
+        using RunResult = util::Result<void, EventError>;
+
+    public:
+        virtual ~Scenario() = default;
+        virtual RunResult run(Orchestrator &orch) = 0;
+    };
+}
+
+#endif // INCLUDE_EUNET_CORE_SCENARIO
