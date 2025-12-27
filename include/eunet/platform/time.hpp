298b24f753bef409389419d597c976cb69677b15
爱特小登队 <t202510423998135@eduxiji.net>
2025-12-27 19:30:20 +0800

    feat: 为时间封装添加了格式化输出 API

--- include/eunet/platform/time.hpp
+++ include/eunet/platform/time.hpp
@@ -9,11 +9,13 @@ namespace platform::time
     using MonoClock = std::chrono::steady_clock;
     using MonoPoint = MonoClock::time_point;
     MonoPoint monotonic_now();
+    std::string to_string(MonoPoint tp);
 
     // 现实时间（系统时间）
     using WallClock = std::chrono::system_clock;
     using WallPoint = WallClock::time_point;
     WallPoint wall_now();
+    std::string to_string(WallPoint tp);
 
     // 时间间隔
     using Duration = std::chrono::nanoseconds;
@@ -30,6 +32,7 @@ namespace platform::time
 
     void sleep_for(Duration d);
     void sleep_until(MonoPoint tp);
+
 }
 
 #endif // INCLUDE_EUNET_PLATFORM_TIME
