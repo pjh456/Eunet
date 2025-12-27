b2d0a2b79a2105a37bb0fa52c840b17706bfb045
爱特小登队 <t202510423998135@eduxiji.net>
2025-12-28 05:26:45 +0800

    feat: 添加了 Orchestrator 封装

---
+++ include/eunet/core/orchestrator.hpp
@@ -0,0 +1,38 @@
+#ifndef INCLUDE_EUNET_CORE_ORCHESTRATOR
+#define INCLUDE_EUNET_CORE_ORCHESTRATOR
+
+#include <vector>
+#include <mutex>
+
+#include "eunet/core/timeline.hpp"
+#include "eunet/core/lifecycle_fsm.hpp"
+#include "eunet/core/i_event_sink.hpp"
+
+namespace core
+{
+    class Orchestrator
+    {
+    private:
+        Timeline timeline;
+        FsmManager fsm_manager;
+
+        std::vector<sink::IEventSink *> sinks;
+
+        mutable std::mutex mtx;
+
+    public:
+        Orchestrator() = default;
+
+    public:
+        const Timeline &get_timeline() const noexcept;
+        const LifecycleFSM *get_fsm(int fd) const;
+
+    public:
+        void emit(Event e);
+
+        void attach(sink::IEventSink *sink);
+        void detach(sink::IEventSink *sink);
+    };
+}
+
+#endif // INCLUDE_EUNET_CORE_ORCHESTRATOR
