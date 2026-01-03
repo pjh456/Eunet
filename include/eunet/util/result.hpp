22a6dd0c682e110c1165d8ff2977d4388c09fda7
爱特小登队 <t202510423998135@eduxiji.net>
2026-01-04 05:16:46 +0800

    docs: 更新了 Result 的代码注释

--- include/eunet/util/result.hpp
+++ include/eunet/util/result.hpp
@@ -48,6 +48,18 @@ namespace util
         };
     }
 
+    /**
+     * @brief 结果封装类 (Monad)
+     *
+     * 提供类似 Rust 的 Result<T, E> 机制，强制调用者处理错误。
+     * 它要么包含一个成功的值 T，要么包含一个错误 E。
+     *
+     * @tparam T 成功时的数据类型
+     * @tparam E 失败时的错误类型
+     *
+     * @note 禁止忽略该函数的返回值 ([[nodiscard]])
+     * @note 禁止 T 与 E 相同
+     */
     template <typename T, typename E>
         requires result_helper::ValidResultTypes<T, E> &&
                  result_helper::NotResult<T> &&
@@ -99,6 +111,12 @@ namespace util
         bool is_err() const noexcept { return std::holds_alternative<E>(data); }
 
     public:
+        /**
+         * @brief 如果是 Ok，则解包获取值；如果是 Err，则抛出异常
+         *
+         * @throw result_helper::bad_result_access 如果当前是 Err 状态
+         * @return T& 内部值的引用
+         */
         [[nodiscard]] T &unwrap() &
         {
             if (!is_ok())
@@ -108,6 +126,12 @@ namespace util
             return std::get<T>(data);
         }
 
+        /**
+         * @brief 如果是 Ok，则解包获取值；如果是 Err，则抛出异常
+         *
+         * @throw result_helper::bad_result_access 如果当前是 Err 状态
+         * @return const T& 内部值的常量引用
+         */
         [[nodiscard]] const T &unwrap() const &
         {
             if (!is_ok())
@@ -117,6 +141,12 @@ namespace util
             return std::get<T>(data);
         }
 
+        /**
+         * @brief 如果是 Ok，则解包获取值；如果是 Err，则抛出异常
+         *
+         * @throw result_helper::bad_result_access 如果当前是 Err 状态
+         * @return T&& 内部值的右值引用
+         */
         [[nodiscard]] T unwrap() &&
         {
             if (!is_ok())
@@ -125,11 +155,23 @@ namespace util
             return std::move(std::get<T>(data));
         }
 
+        /**
+         * @brief 如果是 Ok，则解包获取值；如果是 Err，则使用默认值 val
+         *
+         * @param val 为 Err 的默认返回值
+         * @return const T& 内部值的常量引用或默认值
+         */
         [[nodiscard]] const T &unwrap_or(const T &val) const
         {
             return is_ok() ? std::get<T>(data) : val;
         }
 
+        /**
+         * @brief 如果是 Err，则解包获取值；如果是 Ok，则抛出异常
+         *
+         * @throw result_helper::bad_result_access 如果当前是 Ok 状态
+         * @return E& 内部值的引用
+         */
         [[nodiscard]] E &unwrap_err() &
         {
             if (!is_err())
@@ -139,6 +181,12 @@ namespace util
             return std::get<E>(data);
         }
 
+        /**
+         * @brief 如果是 Err，则解包获取值；如果是 Ok，则抛出异常
+         *
+         * @throw result_helper::bad_result_access 如果当前是 Ok 状态
+         * @return const E& 内部值的常量引用
+         */
         [[nodiscard]] const E &unwrap_err() const &
         {
             if (!is_err())
@@ -148,6 +196,12 @@ namespace util
             return std::get<E>(data);
         }
 
+        /**
+         * @brief 如果是 Err，则解包获取值；如果是 Ok，则抛出异常
+         *
+         * @throw result_helper::bad_result_access 如果当前是 Ok 状态
+         * @return E&& 内部值的右值引用
+         */
         [[nodiscard]] E unwrap_err() &&
         {
             if (!is_err())
@@ -156,12 +210,28 @@ namespace util
             return std::move(std::get<E>(data));
         }
 
+        /**
+         * @brief 如果是 Err，则解包获取值；如果是 Ok，则使用默认值 err
+         *
+         * @param err 为 Ok 的默认返回值
+         * @return const E& 内部值的常量引用或默认值
+         */
         [[nodiscard]] const E &unwrap_err_or(const E &err) const
         {
             return is_err() ? std::get<E>(data) : err;
         }
 
     public:
+        /**
+         * @brief 转换成功值 (Map)
+         *
+         * 如果当前是 Ok(v)，则应用函数 f(v) 并返回 Result::Ok(f(v))。
+         * 如果当前是 Err(e)，则直接返回 Result::Err(e)。
+         *
+         * @tparam F 转换函数类型
+         * @param f 接受 T 返回 U 的可调用对象
+         * @return Result<U, E> 新的结果类型
+         */
         template <typename F>
             requires std::invocable<F, T>
         [[nodiscard]] auto map(F &&f) const
@@ -182,19 +252,16 @@ namespace util
                 return Result<U, E>::Err(std::get<E>(data));
         }
 
-        template <typename F>
-            requires std::invocable<F, T> &&
-                     std::is_same_v<
-                         std::invoke_result_t<F, T>,
-                         E>
-        [[nodiscard]] E map_or(F &&f) const
-        {
-            if (is_ok())
-                return std::invoke(f, std::get<T>(data));
-            else
-                return std::get<E>(data);
-        }
-
+        /**
+         * @brief 转换报错值 (Map_Err)
+         *
+         * 如果当前是 Err(e)，则应用函数 f(e) 并返回 Result::Err(f(e))。
+         * 如果当前是 Ok(v)，则直接返回 Result::Ok(v)。
+         *
+         * @tparam F 转换函数类型
+         * @param f 接受 E 返回 G 的可调用对象
+         * @return Result<T, G> 新的结果类型
+         */
         template <typename F>
             requires std::invocable<F, E>
         [[nodiscard]] auto map_err(F &&f) const
@@ -215,20 +282,15 @@ namespace util
                 return Result<T, E2>::Ok(std::get<T>(data));
         }
 
-        template <typename F>
-            requires std::invocable<F, E> &&
-                     std::is_same_v<
-                         std::invoke_result_t<F, E>,
-                         T>
-        [[nodiscard]] T map_err_or(F &&f) const
-        {
-            if (is_err())
-                return std::invoke(f, std::get<E>(data));
-            else
-                return std::get<T>(data);
-        }
-
     public:
+        /**
+         * @brief 链式调用 (FlatMap / AndThen)
+         *
+         * 如果当前是 Ok(v)，则调用 f(v)，f 必须返回一个 Result 类型。
+         * 用于串联多个可能失败的操作。
+         *
+         * @param f 接受 T 返回 Result<U, E> 的可调用对象
+         */
         template <typename F>
         auto and_then(F &&f)
             const & -> std::invoke_result_t<F, const T &>
@@ -252,6 +314,19 @@ namespace util
         }
     };
 
+    /**
+     * @brief 结果封装类 (Monad)
+     *
+     * 提供类似 Rust 的 Result<T, E> 机制，强制调用者处理错误。
+     * 它要么包含一个成功的值 T，要么包含一个错误 E。
+     * 该版本为 T = void 的特化版本。
+     *
+     * @tparam void 成功时的数据类型
+     * @tparam E 失败时的错误类型
+     *
+     * @note 禁止忽略该函数的返回值 ([[nodiscard]])
+     * @note 禁止 void 与 E 相同
+     */
     template <typename E>
         requires result_helper::ValidResultTypes<void, E> &&
                  result_helper::NotResult<E>
@@ -293,6 +368,11 @@ namespace util
         bool is_err() const noexcept { return std::holds_alternative<E>(data); }
 
     public:
+        /**
+         * @brief 如果是 Ok，则解包；如果是 Err，则抛出异常
+         *
+         * @throw result_helper::bad_result_access 如果当前是 Err 状态
+         */
         void unwrap() const
         {
             if (is_err())
@@ -300,6 +380,12 @@ namespace util
                     "Result<void>::unwrap() called on Err");
         }
 
+        /**
+         * @brief 如果是 Err，则解包获取值；如果是 Ok，则抛出异常
+         *
+         * @throw result_helper::bad_result_access 如果当前是 Ok 状态
+         * @return E& 内部值的引用
+         */
         [[nodiscard]] E &unwrap_err() &
         {
             if (!is_err())
@@ -309,6 +395,12 @@ namespace util
             return std::get<E>(data);
         }
 
+        /**
+         * @brief 如果是 Err，则解包获取值；如果是 Ok，则抛出异常
+         *
+         * @throw result_helper::bad_result_access 如果当前是 Ok 状态
+         * @return const E& 内部值的常量引用
+         */
         [[nodiscard]] const E &unwrap_err() const &
         {
             if (!is_err())
@@ -318,6 +410,12 @@ namespace util
             return std::get<E>(data);
         }
 
+        /**
+         * @brief 如果是 Err，则解包获取值；如果是 Ok，则抛出异常
+         *
+         * @throw result_helper::bad_result_access 如果当前是 Ok 状态
+         * @return E&& 内部值的右值引用
+         */
         [[nodiscard]] E unwrap_err() &&
         {
             if (!is_err())
@@ -326,12 +424,28 @@ namespace util
             return std::move(std::get<E>(data));
         }
 
+        /**
+         * @brief 如果是 Err，则解包获取值；如果是 Ok，则使用默认值 err
+         *
+         * @param err 为 Ok 的默认返回值
+         * @return const E& 内部值的常量引用或默认值
+         */
         [[nodiscard]] const E &unwrap_err_or(const E &err) const
         {
             return is_err() ? std::get<E>(data) : err;
         }
 
     public:
+        /**
+         * @brief 转换成功值 (Map)
+         *
+         * 如果当前是 Ok(void)，则应用函数 f(void) 并返回 Result::Ok(f(void))。
+         * 如果当前是 Err(e)，则直接返回 Result::Err(e)。
+         *
+         * @tparam F 转换函数类型
+         * @param f 接受 void 返回 U 的可调用对象
+         * @return Result<U, E> 新的结果类型
+         */
         template <typename F>
             requires std::invocable<F>
         auto map(F &&f) const
@@ -357,19 +471,16 @@ namespace util
             }
         }
 
-        template <typename F>
-            requires std::invocable<F> &&
-                     std::is_same_v<
-                         std::invoke_result_t<F>,
-                         E>
-        [[nodiscard]] E map_or(F &&f) const
-        {
-            if (is_ok())
-                return std::invoke(f);
-            else
-                return std::get<E>(data);
-        }
-
+        /**
+         * @brief 转换报错值 (Map_Err)
+         *
+         * 如果当前是 Err(e)，则应用函数 f(e) 并返回 Result::Err(f(e))。
+         * 如果当前是 Ok(void)，则直接返回 Result::Ok(void)。
+         *
+         * @tparam F 转换函数类型
+         * @param f 接受 E 返回 G 的可调用对象
+         * @return Result<void, G> 新的结果类型
+         */
         template <typename F>
             requires std::invocable<F, E>
         auto map_err(F &&f) const
@@ -386,18 +497,15 @@ namespace util
                 return Result<void, E2>::Ok();
         }
 
-        template <typename F>
-            requires std::invocable<F, E> &&
-                     std::is_same_v<
-                         std::invoke_result_t<F, E>,
-                         void>
-        void map_err_or(F &&f) const
-        {
-            if (is_err())
-                std::invoke(f, std::get<E>(data));
-        }
-
     public:
+        /**
+         * @brief 链式调用 (FlatMap / AndThen)
+         *
+         * 如果当前是 Ok(v)，则调用 f(v)，f 必须返回一个 Result 类型。
+         * 用于串联多个可能失败的操作。
+         *
+         * @param f 接受 T 返回 Result<U, E> 的可调用对象
+         */
         template <typename F>
             requires result_helper::
                 ResultType<std::invoke_result_t<F>>
